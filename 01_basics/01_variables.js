459d98bd4739c20e8a8498b357227e6f77fe4b32
Aditya-1825 <162556731+Aditya-1825@users.noreply.github.com>
2024-03-07 10:25:51 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 160335
+let  accountEmail = "adita@google.com"
+var  accountPassword ="852369"
+accountCity = "Nagpur"
+
+//  accountId = 5 // not allowed
+
+accountEmail = "ab@bk.com"
+accountPassword = "5656565"
+accountCity = "Vancouver"
+
+ console.log(accountId);
+
+ console.table([accountId,accountEmail,accountPassword,accountCity])
